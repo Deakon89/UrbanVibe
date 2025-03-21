75ac44d1813fa699d70087146a2b03e3caf17641
Deakon89 <franck.devw@gmail.com>
2025-03-21 13:53:23 +0100

    project first config
    setup and first components

---
+++ App/App/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { CommentsComponent } from './component/comments/comments.component';
+import { ProfileComponent } from './component/profile/profile.component';
+;
+
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'comments', component: CommentsComponent},
+    {path: 'profile', component: ProfileComponent},  
+];
+
+
+
+
